b62d8b669fe3fec42f56daff28d32a58dae73077
m-triple-m <triplem656@gmail.com>
2021-07-20 15:43:45 +0530

    ..

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.title('Sentiment Analysis on Covid-19 Vaccine via Social Media')
 st.text("")
 st.text("")
 
-st.image('logo.jpg')
+st.image('logo.jpg', use_column_width=True)
 st.markdown("---")
 sidebar = st.sidebar
 sidebar.title('Sentiment Analysis on Covid-19 Vaccine via Social Media')
@@ -63,10 +63,12 @@ def analyseTweets():
         st.header('Twitter Accounts')
         # st.plotly_chart(plotBar(analysis.getDataframe(), 'user_verified', "Verified vs unverified"))
         # st.write(analysis.getDataframe())
-        st.plotly_chart(piechart(analysis.getDataframe()))
-        st.plotly_chart(add_tracePlot(analysis.getDataframe(),))
+        st.plotly_chart(piechart(analysis.getDataframe()),
+                        use_container_width=True)
+        st.plotly_chart(add_tracePlot(analysis.getDataframe(),),
+                        use_container_width=True)
         st.plotly_chart(barplot1(analysis.getDataframe(), 'user_name', 'total_engagement',
-                                 'total_engagement', 'Viridis', 'Accounts per Engagements'))
+                                 'total_engagement', 'Viridis', 'Accounts per Engagements'), use_container_width=True)
 
 
 def analyseTweets():
@@ -74,16 +76,16 @@ def analyseTweets():
         st.header('Verified vs Unverified Accounts')
         data = analysis.getVerified()
         st.plotly_chart(plotPie(['Unverified Accounts', 'Verified Accounts'], data.values,
-                                'Most of the Tweets are from Non-Verified Accounts'))
+                                'Most of the Tweets are from Non-Verified Accounts'), use_container_width=True)
 
         st.header('Tweet Engagements on the basis of Followers')
         st.plotly_chart(barplot(analysis.getDataframe(), 'acc_class', 'total_engagement',
-                                'total_engagement', 'Rainbow', 'Most of the responses are given by popular accounts with huge audience following'))
+                                'total_engagement', 'Rainbow', 'Most of the responses are given by popular accounts with huge audience following'), use_container_width=True)
 
         st.header('Media vs No Media Tweets')
         data = analysis.getDataframe()
         st.plotly_chart(plotPie(['Media', 'No Media'], data.groupby(
-            'med').count()['user_name'].values, 'Almost all the Responses contained Media such as Images, Videos, Gifs etc.'))
+            'med').count()['user_name'].values, 'Almost all the Responses contained Media such as Images, Videos, Gifs etc.'), use_container_width=True)
 
         st.header('Length of Tweets')
 
@@ -91,20 +93,21 @@ def analyseTweets():
 def analyseSentiments():
     with st.spinner("Loading Analysis..."):
         st.header('Most Used words in Tweets')
-        st.image('plotImages/Cloud1.png')
+        st.image('plotImages/Cloud1.png', use_column_width=True)
 
         st.header('Most Used words in Tweets')
-        st.image('plotImages/Cloud1_bar.png')
+        st.image('plotImages/Cloud1_bar.png', use_column_width=True)
 
         st.header('Most Used words in Tweets')
-        st.image('plotImages/Cloud1.png')
+        st.image('plotImages/Cloud1.png', use_column_width=True)
 
         st.header('Sentiment Count')
         data = analysis.getPolarityCount()
-        st.plotly_chart(plotPie(data.index, data.values, 'title'))
+        st.plotly_chart(plotPie(data.index, data.values,
+                                'title'), use_container_width=True)
 
         st.plotly_chart(barplot(analysis.getEngagementSentiment(
-        ), 'total_engagement', 'sentiment', 'total_engagement', 'Rainbow', 'title'))
+        ), 'total_engagement', 'sentiment', 'total_engagement', 'Rainbow', 'title'), use_container_width=True)
 
 
 sidebar.header('Choose Your Option')
